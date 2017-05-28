fa9886501fb2777cd5248c516bc4605ce1324376
gauravvichare <gauravvichare99@gmail.com>
2017-05-28 11:53:29 +0530

    added test script

---
+++ test_amth_op.py
@@ -0,0 +1,17 @@
+import math_op
+
+
+class TestMathOP(object):
+    """
+    """
+
+    def test_add_op():
+        """
+        """
+        assert math_op.add(10, 20) == 30
+
+
+    def test_mul_op():
+        """
+        """
+        assert math_op.mul(10, 20) == 200
